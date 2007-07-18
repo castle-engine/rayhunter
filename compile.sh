a7285cce328d826838c9fd0f5e0fa37daf93bb8c
Michalis Kamburelis <michalis.kambi@gmail.com>
2007-07-18 15:44:48 +0000

    + compile.sh scripts added to SVN to comfortably compile all * svn:ignore set on many dirs to ignore executables created there by compile.sh scripts
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/rayhunter@1813 afe8dcc5-9094-451b-8773-5f495042a737

---
+++ compile.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+set -eu
+
+# This is automatically generated script that should compile
+# all programs in this archive. It simply calls FPC
+# with proper command-line options.
+#
+# We must do cd ../kambi_vrml_game_engine/ (and call FPC from that directory)
+# because kambi.cfg file is there and it contains paths relative
+# to that directory.
+
+cd ../kambi_vrml_game_engine/
+
+fpc -dRELEASE @kambi.cfg ../rayhunter/rayhunter.dpr
