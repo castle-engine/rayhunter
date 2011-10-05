bb9103faf2afa6d6636681d8ff0096bed11ad67b
Michalis Kamburelis <michalis.kambi@gmail.com>
2011-10-05 02:53:46 +0000

    Rename kambi.cfg -> castle-fpc.cfg, KAMBI_FPC_OPTIONS -> CASTLE_FPC_OPTIONS, fix comments in many xxx_compile.sh scripts.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/rayhunter@10168 afe8dcc5-9094-451b-8773-5f495042a737

--- compile.sh
+++ compile.sh
@@ -6,9 +6,9 @@ set -eu
 # with proper command-line options.
 #
 # We must do cd ../castle_game_engine/ (and call FPC from that directory)
-# because kambi.cfg file is there and it contains paths relative
+# because castle-fpc.cfg file is there and it contains paths relative
 # to that directory.
 
 cd ../castle_game_engine/
 
-fpc -dRELEASE "${KAMBI_FPC_OPTIONS:-}" @kambi.cfg ../rayhunter/rayhunter.lpr
+fpc -dRELEASE "${CASTLE_FPC_OPTIONS:-}" @castle-fpc.cfg ../rayhunter/rayhunter.lpr
